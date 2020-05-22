0533f24f36a77ac63b34c3907706b124ec717da5
albertdrouart <albert.drouart@gmail.com>
2020-05-21 23:10:07 -0400

    used the cachemean example to make an function to store both a matrix and its inverse so that the identity matrix wouldn't have to be recalculated every time.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functios work together to return and cache the inverse of a matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates an object where the reusable inverse can be stored
+## ("cached") and provides a set of utility functions to obtain that data once
+## set.   When passed an Matrix the function will store the value in x which is
+## the data, and once the identity matrix is cached will store the value in m
 
+setmakeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(slv) m <<- slv
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+#cache solve is passed first the CacheMatrix -- that is a matrix which captures
+#state of the underlying matrix and it's identity.  First it checks if the
+#CacheMatrix # already has a value.   If it does it will return that value,
+#otherwise it will solve it then # set the value of the CacheMatrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached solve data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
